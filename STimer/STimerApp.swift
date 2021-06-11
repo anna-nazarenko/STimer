b321bdcd4b463302dd3e57c68d54e4303c526d35
Karma <anna.nazarenko0@gmail.com>
2021-06-11 15:28:17 +0300

    Initial Commit

---
+++ STimer/STimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  STimerApp.swift
+//  STimer
+//
+//  Created by Anna on 11.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct STimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
